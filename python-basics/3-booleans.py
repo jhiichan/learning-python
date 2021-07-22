86917b19620ab8dd92db7865f789894b27c7c379
Lloyd John Borres <eljayzborz@gmail.com>
2021-07-22 13:26:53 +0800

    Update python basics - boolean

--- python-basics/3-booleans.py
+++ python-basics/3-booleans.py
@@ -1,18 +1,34 @@
 # Boolean and Conditional
 
-# If statement
+# Uppercase letters are smaller than lowercase letters
+print('M' < 'm') #True
+
+# digits are smaller than letters
+print('1' < 'a') #True
+
+# True has a value of 1
+print(True == 1) #true
+print(True + True) #2
+print(True + 3) #4
+
+# False has a value of 0
+print(False == 0) #true
+print(False + False) #0
+print(False + True) #1
+
+# if statement
 sleepy = True
 if sleepy:
     print('Get Coffee!')
 
-# If Else statement
+# if else statement
 sleepy = False
 if sleepy:
     print('Get Coffee!')
 else:
     print('Continue what you are doing...')
 
-# Else If Statement
+# else if Statement
 day = 'Sunday'
 if day == 'Saturday':
     print('Freedom!')
