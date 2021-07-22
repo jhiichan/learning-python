0d9dcc8a7fee396a8294909116a28bf44890340a
Lloyd John Borres <eljayzborz@gmail.com>
2021-07-22 10:15:09 +0800

    Update readme and add python basics - numbers

---
+++ python-basics/numbers.py
@@ -0,0 +1,30 @@
+# Numbers
+
+# int
+a = 10
+
+# float
+b = 2.0
+
+# calculated
+c = a * b
+
+# convert float to int
+d = int(b)
+
+# convert int to float
+e = float(a)
+
+# Printing
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+# Printing Data Types
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
