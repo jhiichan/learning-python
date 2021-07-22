02deb90baadcdcbd29d6ac5c75b77674c19b07db
Lloyd John Borres <eljayzborz@gmail.com>
2021-07-22 13:27:04 +0800

    Add python basics - loops

---
+++ python-basics/4-loops.py
@@ -0,0 +1,14 @@
+# Note: To get out of an infinite loop, you can press control + c
+
+# For Loop
+
+for letter in 'Hello':
+    print(letter)
+
+
+# While Loop
+
+i = 1
+while i <= 4: # while statement is True, continue to execute loop
+    print(i)
+    i += 1
