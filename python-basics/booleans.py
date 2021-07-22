e4b7273717deb9e923d5b87c15a56158a5f62f5d
Lloyd John Borres <eljayzborz@gmail.com>
2021-07-22 11:28:49 +0800

    Add python basics - booleans

---
+++ python-basics/booleans.py
@@ -0,0 +1,22 @@
+# Boolean and Conditional
+
+# If statement
+sleepy = True
+if sleepy:
+    print('Get Coffee!')
+
+# If Else statement
+sleepy = False
+if sleepy:
+    print('Get Coffee!')
+else:
+    print('Continue what you are doing...')
+
+# Else If Statement
+day = 'Sunday'
+if day == 'Saturday':
+    print('Freedom!')
+elif day == 'Sunday':
+    print('Shopping!')
+else:
+    print ('Work!')
