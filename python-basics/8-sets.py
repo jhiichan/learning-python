02cf53014e6aa7150cbc8968a0372d3ed74bb67c
Lloyd John Borres <eljayzborz@gmail.com>
2021-07-23 10:06:19 +0800

    Update sets

--- python-basics/8-sets.py
+++ python-basics/8-sets.py
@@ -12,3 +12,11 @@ my_list = ['water', 'water', (1, 2), (1, 2), 1000, 1000]
 list_to_set = set(my_list)
 print(my_list)
 print(list_to_set)
+
+# comparison
+yorha_2b = {'YoRHa', 'YoRHa Android', 'Female', 'Silvery White Hair', 'Gray Blue Eye', 168, 148.8}
+yorha_9s = {'YoRHa Scanning Unit', 'YoRHa Android', 'Male', 'Silvery White Hair', 'Light Blue Eye', 160, 129.9}
+
+print(yorha_2b.difference(yorha_9s))
+print(yorha_2b.intersection(yorha_9s))
+print(yorha_2b.union(yorha_9s))
