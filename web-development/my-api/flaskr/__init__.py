ef7a088f0e998328f3ab15bfe55480bba7a3052b
Lloyd John Borres <eljayzborz@gmail.com>
2021-07-26 13:26:49 +0800

    Update lab, init and show

--- web-development/my-api/flaskr/__init__.py
+++ web-development/my-api/flaskr/__init__.py
@@ -1,4 +1,4 @@
-import os
+# import os
 
 from flask import Flask
 
@@ -8,23 +8,23 @@ from . import show
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+    # app.config.from_mapping(
+    #     SECRET_KEY='dev',
+    #     DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    # )
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+    # if test_config is None:
+    #     # load the instance config, if it exists, when not testing
+    #     app.config.from_pyfile('config.py', silent=True)
+    # else:
+    #     # load the test config if passed in
+    #     app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    # # ensure the instance folder exists
+    # try:
+    #     os.makedirs(app.instance_path)
+    # except OSError:
+    #     pass
 
     app.register_blueprint(show.bp)
 
--- web-development/my-api/flaskr/show.py
+++ web-development/my-api/flaskr/show.py
@@ -4,6 +4,7 @@ from flask import (
 
 bp = Blueprint('show', __name__, url_prefix='/show')
 
+# Dummy Data
 my_shows = {
     0: {
         'title': 'The Avengers',
@@ -22,23 +23,34 @@ my_shows = {
         'director': '',
         'cast': ['Ed O\'Neill', 'Sofía Vergara', 'Julie Bowen' ,'Ty Burrell'],
         'type': 'series'
+    },
+    3: {
+        'title': 'Modern Family',
+        'director': '',
+        'cast': ['Ed O\'Neill', 'Sofía Vergara', 'Julie Bowen' ,'Ty Burrell'],
+        'type': 'series'
     }
 }
 
+# API to get all shows
 @bp.route('/', methods=('GET',))
 def index():
     return my_shows
 
+# API to get details of one show using ID
 @bp.route('/<int:id>', methods=('GET',))
 def get_by_id(id):
     return my_shows[id]
 
+# API to search shows using a keyword
 @bp.route('/search/<string:keyword>', methods=('GET',))
 def search(keyword):
     result = {}
     for key, value in my_shows.items():
         show = list(value.values())
         if keyword in show:
+            result[key] = my_shows[key]
+        elif keyword in value['cast']:
             result[key] = my_shows[key]
 
     return result
